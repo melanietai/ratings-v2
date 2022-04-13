c9416678dfde1c2b0ce10cbb2a1c6d0c4b5135a2
Melanie Tai <mt337@cornell.edu>
2022-04-13 15:12:42 -0700

    Finish seed_database

---
+++ seed_database.py
@@ -0,0 +1,43 @@
+"""Script to seed database."""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system("dropdb ratings")
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+# Load movie data from JSON file
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+
+# TODO: get the title, overview, and poster_path from the movie
+# dictionary. Then, get the release_date and convert it to a
+# datetime object with datetime.strptime
+
+for movie in movie_data:
+    title = movie["title"]
+    overview = movie["overview"]
+    poster_path = movie["poster_path"]
+    
+    release_date = datetime.strptime(movie["release_date"], '%Y-%m-%d')
+
+# TODO: create a movie here and append it to movies_in_db
+
+    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+    movies_in_db.append(db_movie)
+
+model.db.session.add_all(movies_in_db)
+model.db.session.commit()
