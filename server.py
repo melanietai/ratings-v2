7568e19dab1062cad7466768d1dd9d0bb94d3e09
Melanie Tai <mt337@cornell.edu>
2022-04-14 16:28:47 -0700

    Complete part 4

--- server.py
+++ server.py
@@ -36,6 +36,29 @@ def show_movie(movie_id):
     
     return render_template('movie_details.html', movie = movie)
 
+@app.route('/movies/<movie_id>/rating', methods=["POST"])
+def add_rating(movie_id):
+    """Add rating for specific movie"""
+#check if in session
+
+    if session:
+        score = request.form.get('score')
+        user = crud.get_user_by_email(session['user_email'])
+        movie = crud.get_movie_by_id(movie_id)
+
+
+        rating = crud.create_rating(user, movie, int(score))
+
+        db.session.add(rating)
+        db.session.commit()
+
+        flash(f'Your rating has been added! It is {score}.')
+    else:
+        flash('You must be logged fin, in order to submit a rating.')
+    
+    return redirect(f'/movies/{movie_id}')
+
+
 
 @app.route('/users')
 def all_users():
@@ -44,6 +67,48 @@ def all_users():
     users = crud.get_users()
 
     return render_template('all_users.html', users=users)
+
+
+
+@app.route('/users', methods=["POST"])
+def register_user():
+    """Create a new user."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash("Cannot create an account with that email. Try again.")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+
+    return redirect("/")
+
+
+@app.route('/login', methods=["POST"])
+def login_user():
+    """User logs in"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_email(email)
+
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect!")
+    else:
+        # Log in user by storing the user's email in session
+        session["user_email"] = user.email
+        flash(f"Welcome back, {user.email}!")
+
+    return redirect("/")
+
+
 
 
 @app.route('/users/<user_id>')
